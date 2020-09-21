c407958878f13795ea61e899c68a84c8cec684a9
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-09-21 15:23:01 -0500

    delete comment

--- src/components/MonthItem/MonthItem.js
+++ src/components/MonthItem/MonthItem.js
@@ -4,7 +4,7 @@ class MonthItem extends Component {
     render() {
         return (
             <li 
-            key={this.props.id} // Don't know why I'm still getting the error for 'unique key prop' here because its the same as the song.js file
+            key={this.props.id}
             onClick={() => this.props.onMonthClick(this.props.month)}
             >
                 {this.props.month}
